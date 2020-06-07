f4a75557bf5fd44422697a43fbe728f09fa1df6a
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-07 04:13:50 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,10 @@
 # Write your code here
 
 number_reel = 10
-new_year = "Happy New Year!"
+new_year_exit = 1
 
 while number_reel >= 1 do
+  break if number_reel == new_year_exit && puts "Happy New Year!"
   puts "#{number_reel}"
   number_reel -= 1
 end
