8178155d615459322562e1bb52b227db84d2ebd4
Ashlee Barksdale <ashleelennab@gmail.com>
2020-06-07 04:04:48 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
 
-number_reel = 0
+number_reel = 10
 new_year = "Happy New Year!"
 
 while number_reel <= 10 do
   puts "#{number_reel}"
-  number_reel = number_reel + 1
+  number_reel = number_reel - 1
 end
